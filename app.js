6f4529e8dbdf1377445a5e049c49b07aa7dd8b9b
salmansrabon <salmansrabon@gmail.com>
2025-07-09 00:29:18 +0600

    refactor: consolidate CORS configuration and reorder middleware setup

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+
 const app = express();
+
+// CORS MUST come before routes
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://roadtocareer.net'],
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type']
+}));
+
+app.use(bodyParser.json());
+
 const sdetbotRoutes = require('./roadtosdet/chatbot.route.js');
 const careerbotRoutes = require('./roadtocareer/chatbot.route.js');
 
-
-app.use(cors({ origin: "http://localhost:3000" }));
-app.use(bodyParser.json());
 app.use('/roadtosdet', sdetbotRoutes);
 app.use('/roadtocareer', careerbotRoutes);
+
 module.exports = app;
